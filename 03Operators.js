2f7b47b68218ef96bd59398fd243f8ff083979d3
qureshizainab <qureshizainab591@gmail.com>
2024-07-01 17:51:20 +0530

    msg

---
+++ 03Operators.js
@@ -0,0 +1,8 @@
+var sellingPrice = 199;
+var listPrice = 799;
+var discount = ((listPrice - sellingPrice) / listPrice) * 100;
+console.log("Discount Percentage is : ", discount);
+console.log("Rounded Discount : ", Math.round(discount), " %off");
+
+var result = listPrice > sellingPrice;
+console.log(result);
---
+++ variable 02.js
@@ -0,0 +1,17 @@
+const id = "abc123";
+var fullName = "ZainabQureshi";
+var email = "ZainabQureshi@gmail.com";
+var pwd = "123456";
+var cfmPwd = "123456";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+console.log("Fullname is", fullName);
+
+console.log(`
+      Unique Id is : ${id}
+      Fullname : ${fullName}
+      Email : ${email}
+      Password: ${pwd}
+      
+`)
