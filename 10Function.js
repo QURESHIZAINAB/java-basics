31a19af7647b7daf549296b869e91fda59da6561
qureshizainab <qureshizainab591@gmail.com>
2024-07-10 17:39:05 +0530

    msg

--- 10Function.js
+++ 10Function.js
@@ -36,9 +36,9 @@
 // let userName = 'John';
 
 // function showMessage() {
-  let message = 'Hello, ' + userName;
-  alert(message);
-}
+//   let message = 'Hello, ' + userName;
+//   alert(message);
+// }
 
 // showMessage(); // Hello, John
 
